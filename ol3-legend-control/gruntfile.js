6ec27edb0d53cdc463c9cfae5da7d401f6082099
Poul Kjeldager Sørensen <pks@s-innovations.net>
2014-11-24 16:54:34 +0100

    initial

---
+++ ol3-legend-control/gruntfile.js
@@ -0,0 +1,20 @@
+﻿module.exports = function (grunt) {
+    console.log(grunt);
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.initConfig({
+        typescript: {
+            base: {
+                src: ['path/to/typescript/files/**/*.ts'],
+                dest: 'where/you/want/your/js/files',
+                options: {
+                    module: 'amd', //or commonjs
+                    target: 'es5', //or es3
+                    basePath: 'src',
+                    sourceMap: true,
+                    declaration: true
+                }
+            }
+        },
+    });
+
+}
---
+++ ol3-legend-control/src/ol3-legend-control.js
@@ -0,0 +1,11 @@
+﻿define(["require", "exports"], function(require, exports) {
+    var Test = (function () {
+        function Test() {
+        }
+        return Test;
+    })();
+
+    
+    return Test;
+});
+//# sourceMappingURL=ol3-legend-control.js.map
